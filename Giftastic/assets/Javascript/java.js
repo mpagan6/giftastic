1eb35db290ccaf2a870827a6f83fc7662f2694a0
marilyn pagan <mpagan6@gmail.com>
2018-02-21 13:12:38 -0600

    "updates"

--- Giftastic/assets/Javascript/java.js
+++ Giftastic/assets/Javascript/java.js
@@ -7,26 +7,26 @@
  function displayGiphy() {
     var myAPI= '&api_key=7zHrOq4p15SZAsoqYjWTSZuhHy5YhtK2'
     var giphy = $(this).attr("data-name");
-   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + adultShow + myAPI  +"&limit=10";
+   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giphy + myAPI  +"&limit=10";
 
    // Creating an AJAX call for the specific GIF button being clicked
    $.ajax({
      url: queryURL,
      method: "GET"
    }).then(function(response) {
-       
+       console.log("this is our return", response);
     // clears out prior GIFs
     $("#gif-view").html('');
 
     var counter;
 
-    
+    for (var i= 0; i< response.data.length; i++){
 
      // Creating a div to hold the GIF
      var gifDiv = $("<div class='giphy'>");
 
      // Storing the rating data
-     var rating = response.Rated;
+     var rating = response.data[i].rating;
 
      // Creating an element to have the rating displayed
      var pOne = $("<p>").text("Rating: " + rating);
@@ -35,13 +35,18 @@
      gifDiv.prepend(pOne);
 
       // Creating an element to hold the image
-     var image = $("<img>").attr("src", queryURL);
+     var image = $("<img>").attr("src", response.data[i].images.original.url);
+               image.attr("movingUrl", response.data[i].images.original.url);
+               image.attr("stillUrl", response.data[i].images.original_still.url);
+               image.attr("isMoving", true);
+               image.attr("id", "gifImage");
 
      // Appending the image
      gifDiv.prepend(image);
 
-     // Putting the entire movie above the previous movies
+     // Putting the giphy in div
      $("#gif-view").prepend(gifDiv);
+    }
    });
 
  }
@@ -49,22 +54,23 @@
  // Function for displaying giphy data
  function renderButtons() {
 
-   // Deleting the movies prior to adding new movies
+   // 
    // (this is necessary otherwise you will have repeat buttons)
    $("#buttons-view").empty();
 
    // Looping through the array of movies
-   for (var i = 0; i < gifArray.length; i++) {
-
+   for (var i = 0; i < adultShow.length; i++) {
+    console.log("this is the adultshow" + adultShow[i]);
      // Then dynamicaly generating buttons for each movie in the array
      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
      var a = $("<button>");
      // Adding a class of movie-btn to our button
      a.addClass("gif-btn");
      // Adding a data-attribute
-     a.attr("data-name", gifArray[i]);
+     a.attr("data-name", adultShow[i]);
      // Providing the initial button text
-     a.text(gifArray[i]);
+     a.text(adultShow[i]);
+     console.log("what is being appended", a);
      // Adding the button to the buttons-view div
      $("#buttons-view").append(a);
    }
@@ -74,10 +80,10 @@
  $("#add-giphy").on("click", function(event) {
    event.preventDefault();
    // This line grabs the input from the textbox
-   var giphy = $("#giphy-input").val();
+   var giphy = $("#gif-input").val();
 
-   // Adding movie from the textbox to our array
-   giphy.push(gifArray);
+   // Adding  from the textbox to our array
+   adultShow.push(giphy);
 
    // Calling renderButtons which handles the processing of our movie array
    renderButtons();
@@ -85,6 +91,20 @@
 
  // Adding a click event listener to all elements with a class of "movie-btn"
  $(document).on("click", ".gif-btn", displayGiphy);
+ $(document).on("click", "#gifImage", playPause);
 
  // Calling the renderButtons function to display the intial buttons
- renderButtons();
+ renderButtons()
+
+function playPause (){
+  if ($(this).attr("isMoving") === true){
+    console.log("its moving");
+    $(this).attr("isMoving", false);
+  } else {
+    $(this).attr("isMoving", true);
+    console.log ("it's not moving")
+  }
+  
+}
+
+});
